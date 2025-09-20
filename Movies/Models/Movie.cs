0ddfde7a569086bb8d0d1394615214c10af53ffb
Albert <Izuku@DESKTOP-125A3DF>
2025-09-20 19:30:50 +0500

    SQLServer Update

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Movies.Models
+{
+    public class Movie
+    {
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Priice { get; set; }
+    }
+}
