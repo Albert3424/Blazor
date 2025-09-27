c06901cd3a4fa5da3614c5047ba717c219f73f4b
Albert <Izuku@DESKTOP-125A3DF>
2025-09-27 21:11:48 +0500

    BlazorAcademy in progress

---
+++ BlazorAcademy/Migrations/20250927160953_mssql.local_migration_630.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_630 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directions",
+                columns: table => new
+                {
+                    direction_id = table.Column<byte>(type: "tinyint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    direction_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directions", x => x.direction_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    group_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    group_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    direction = table.Column<byte>(type: "tinyint", nullable: false),
+                    weekdays = table.Column<byte>(type: "tinyint", nullable: false),
+                    start_time = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.group_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    stud_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    group = table.Column<int>(type: "int", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    middle_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.stud_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    teacher_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    work_since = table.Column<DateOnly>(type: "date", nullable: false),
+                    rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    middle_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.teacher_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Directions");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -1,11 +1,27 @@
-﻿namespace Movies.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies.Models
 {
     public class Movie
     {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Title { get; set; }
+
+    [RangeAttribute(typeof(DateTime), "1888-10-14", "2032-12-31")]
     public DateOnly ReleaseDate { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-za-Z()\s-]*$")]
     public string? Genre { get; set; }
+
+    [Range(0, 200)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Priice { get; set; }
     }
 }
